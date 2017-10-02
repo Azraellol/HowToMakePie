0ad730a02a6f37bef807149618e7bef5d519b3bf
Leah Briscoe <leahpb101@gmail.com>
2017-10-01 20:32:01 -0700

    Add files via upload

---
+++ PieChartGeneral.R
@@ -0,0 +1,19 @@
+values <- read.csv("~/Documents/TeachingResources/YouTube/PieChart/ToppingMoviePreference.csv",row.names=1, check.names=FALSE,header=TRUE)
+values2 <- read.csv("~/Documents/TeachingResources/YouTube/PieChart/OD_Data.csv",row.names=1, header=TRUE)
+
+require("RColorBrewer")
+indices <- values[,1]!=0
+pie(values[,1][indices],labels=row.names(values)[indices],col=brewer.pal(length(values[,1][indices]),'Spectral'))
+legend("topleft",legend=row.names(values)[indices],fill=brewer.pal(length(values[,1][indices]),'Spectral'))
+
+for(c in 1:ncol(values)){
+  indices <- values[,c]!=0
+  par(mar=c(3,4,2,3))
+  pie(values[,c][indices],labels=row.names(values)[indices],col=brewer.pal(length(values[,c][indices]),'Spectral'),main=colnames(values)[c])
+  legend("topleft",legend=row.names(values)[indices],fill=brewer.pal(length(values[,c][indices]),'Spectral'),cex=0.6)
+  
+}
+
+# other color options:
+# grey.colors(nrow(values))
+# brewer.pal(nrow(values),"Paired")
